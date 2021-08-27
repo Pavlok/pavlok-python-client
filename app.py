260ef426c0da1adfcca9b0f7be4f27ce1f4f9c9d
maneeshsethi <maneesh@maneeshsethi.com>
2021-08-27 07:55:03 -0400

    Added basic session management and redirect on no key.

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, jsonify, url_for, session
+from flask import Flask, jsonify, url_for, session, redirect
 from flask_session import Session
 from authlib.integrations.flask_client import OAuth
 
 class Pavlok:
     token: int = None
 
+        
+    def set(self,value):
+        session['pavlok-token'] = value
+        return 'ok'
+
+
+    def get(self):
+        return session.get('pavlok-token')
+    
+    def clear(self):
+        session['pavlok-token'] = None
+        return 'ok'
+    
+    
 app = Flask(__name__)
 app.secret_key = "DiFAdW#8UNVR%M"
 oauth = OAuth(app)
-github = oauth.register('github', {...})
+
+# SESSION_TYPE = 'filesystem'
+# app.config.from_object(__name__)
+# Session(app)
+
+# @app.route('/set/<key>/<value>')
+# def set(key,value):
+#     session[key] = value
+#     return 'ok'
+
+# @app.route('/get/<key>')
+# def get(key):
+#     return session.get(key)
+
 oauth.register(
     name='pavlok',
     client_id='f4037a18271857d6512700426dfe93ae80e96033c55017c84989adad67ea6087',
@@ -39,13 +66,23 @@ def authorize():
     profile = pavlok_client.get('/user', token=token)
     #return jsonify(profile)
     print(profile)
+    pavlok.set(pavlok.token)
     return pavlok.token
 
+@app.route("/logout")
+def logout():
+    return pavlok.clear()
 @app.route("/vibrate")
 def vibrate():
-   v = pavlok_client.post('/api/v1/stimuli/vibration/200',  token=pavlok.token) #reason="API /vibrate sent!",
-   print(v)
-   return (v.text) 
+    sess = pavlok.get()
+    if sess is None:
+        return redirect("/login")
+        #return url_for('login', _external=True)
+    else: 
+        pavlok.token = sess
+    v = pavlok_client.post('/api/v1/stimuli/vibration/200',  token=pavlok.token) #reason="API /vibrate sent!",
+    print(v)
+    return (v.text) 
 @app.route('/get_token')
 def get_token():
     return pavlok.token
