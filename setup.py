2873f55ffbb61fd3fdb9d1ce22a8c18950b78de4
maneeshsethi <maneesh@maneeshsethi.com>
2021-09-07 17:11:51 +0300

    packaging for pypi

--- setup.py
+++ setup.py
@@ -2,17 +2,24 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='example_publish_pypi_medium',
-    version='0.6',
+    name='Pavlok Python Client',
+    version='0.1.0',
     license='MIT',
-    author="Giorgos Myrianthous",
-    author_email='email@example.com',
+    author="Maneesh Sethi",
+    author_email='maneesh@pavlok.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
-    keywords='example project',
+    url='https://github.com/Pavlok/pavlok-python-client',
+    keywords='pavlok',
     install_requires=[
-          'scikit-learn',
+        'Authlib==0.15.4',
+        'fastapi==0.65.3',
+        'pydantic==1.8.2',
+        'python-dotenv==0.19.0',
+        'starlette==0.15.0',
+        'starsessions==1.1.0',
+        'urllib3==1.26.6',
+        'uvicorn==0.15.0'
       ],
 
 )
