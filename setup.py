2cb6517c0e89c09000bda2cc5f64b3b8a99802f8
Ganesh <justganesh12321@gmail.com>
2021-09-23 21:06:00 +0530

    client implementation

--- setup.py
+++ setup.py
@@ -2,24 +2,21 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='pavlok',
-    version='0.1.0',
-    license='MIT',
+    name="pavlok",
+    version="0.1.0",
+    license="MIT",
     author="Maneesh Sethi",
-    author_email='maneesh@pavlok.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/Pavlok/pavlok-python-client',
-    keywords='pavlok',
+    author_email="maneesh@pavlok.com",
+    packages=find_packages("pavlok"),
+    package_dir={"": "pavlok"},
+    url="https://github.com/Pavlok/pavlok-python-client",
+    keywords="pavlok",
     install_requires=[
-        'Authlib==0.15.4',
-        'fastapi',
-        'pydantic==1.8.2',
-        'python-dotenv==0.19.0',
-        'starlette',
-        'starsessions==1.1.0',
-        'urllib3==1.26.6',
-        'uvicorn==0.15.0'
-      ],
-
+        "Authlib==0.15.4",
+        "fastapi==0.68.1",
+        "pydantic==1.8.2",
+        "python-dotenv==0.19.0",
+        "starlette==0.14.2",
+        "uvicorn==0.15.0",
+    ],
 )
