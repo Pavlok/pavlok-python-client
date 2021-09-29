cbb97ad68da3877b67ab82321292fb6894685c45
Ganesh <justganesh12321@gmail.com>
2021-09-29 13:59:23 +0530

    version upgrade patch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pavlok',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     author="Maneesh Sethi",
     author_email='maneesh@pavlok.com',
